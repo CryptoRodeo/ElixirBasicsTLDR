3cf0879462b66e03629bfd19a23e880a6c825f22
cryptorodeo <b.r.tech95@gmail.com>
2020-04-26 07:20:38 -0400

    adding nested data structure example

--- keyword_lists_n_maps.exs
+++ keyword_lists_n_maps.exs
@@ -34,3 +34,54 @@ IO.inspect other_list ++ [protien_bar: "sure!"]
 #
 #MAPS:
 #
+# Whenever you need a key-value store, use maps!
+
+diet = %{
+  :donut => "mmm...donuts",
+  :soda  => "diet coke please"
+}
+
+IO.puts diet[:donut]
+
+#maps allow any value as a key and do not follow any ordering.
+#
+#Updating map values:
+#
+#Note: A key has to exist. This can not be used for adding values to the map.
+
+real_diet = %{diet | :donut => "Actually i'll have a salad"}
+
+IO.puts real_diet[:donut]
+
+#atom keys can also be accessed using the . operator
+
+IO.puts real_diet.soda
+
+
+#Nested Data Structures
+#You can have maps inside maps, keyword lists inside maps and etcetera.
+
+#Here is a keyword list of users where each value is a map containing their:
+#name, age, and programming languages they like.
+users = [
+  john: %{name: "John", age: 25, languages: ["C#","Ruby","Elixir"]},
+  ben: %{name: "Ben", age: 28, languages: ["F#", "Ruby", "C++"]}
+]
+
+#To access john's age we can do
+IO.puts users[:john].age
+
+#We can also use the above syntax to update values using the put_in macro
+
+users = put_in users[:john].age, 26 #happy birthday
+
+IO.puts users[:john].age
+
+#We can also use the update_in macro, this allows us to pass in a function
+#that controls how the values change
+
+users = update_in users[:ben].languages, fn languages ->
+  List.delete(languages, "F#")
+end
+
+IO.inspect users[:ben].languages
